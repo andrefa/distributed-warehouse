e3b5dac77ed9d92946c2a87b1babeeb12ccc9ec1
jose.ricardo.k <jose.ricardo.k@9075f21e-a35f-48da-a654-f657207f4b71>
2011-12-03 13:15:15 +0000

    CRUJ

---
+++ DiswahServer/src/br/furb/diswah/storage/EntityStorageImpl.java
@@ -0,0 +1,33 @@
+package br.furb.diswah.storage;
+
+/**
+ * 
+ * @author André Felipe de Almeida {almeida.andref@gmail.com}
+ */
+public class EntityStorageImpl extends EntityStoragePOA{
+
+	@Override
+	public String save(String p1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String find(int p1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String delete(int p1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String list() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
